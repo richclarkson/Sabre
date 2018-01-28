a373c55602c458e901fb0cfb9a23089597e94ee3
Steve Faletti <stevefaletti@gmail.com>
2018-01-28 12:36:31 -0500

    add teensy tests

--- test/test_saber_settings/TestSaberSettings.cpp
+++ test/test_saber_settings/TestSaberSettings.cpp
@@ -1,30 +1,45 @@
 #if defined(UNIT_TEST) && defined(ARDUINO)
 
 #include "StateManager.h"
+#include <Arduino.h>
 #include <unity.h>
 
 StateManager sm;
 
-void setUp() {
-    sm = StateManager();
-}
+void setUp() { sm = StateManager(); }
 
 void test_settings_exist() {
     Serial.begin(9600);
+    sm.resetSettings();
     Serial.print("Channel: ");
     Serial.println(sm.getChannel());
+    TEST_ASSERT_EQUAL(8, sm.getChannel());
     Serial.print("Sensitivity: ");
     Serial.println(sm.getSensitivity());
+    TEST_ASSERT_EQUAL(4, sm.getSensitivity());
     Serial.print("Brightness: ");
     Serial.println(sm.getBrightness());
+    TEST_ASSERT_EQUAL(4, sm.getBrightness());
 }
 
-int main() {
+void test_set_settings() {
+  sm.resetSettings();
+  TEST_ASSERT_EQUAL(4, sm.getSensitivity());
+  TEST_ASSERT_EQUAL(4, sm.getBrightness());
+  TEST_ASSERT_EQUAL(8, sm.getChannel());
+}
+
+void setup() {
+
   UNITY_BEGIN();
+  delay(2000);
 
   RUN_TEST(test_settings_exist);
+  RUN_TEST(test_set_settings);
 
   UNITY_END();
 }
+
+void loop() {}
 
 #endif
