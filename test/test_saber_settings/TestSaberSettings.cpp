34111110c5de3e7a4e75addcaf5b5a042cc23de9
Steve Faletti <stevefaletti@gmail.com>
2018-01-28 12:08:08 -0500

    add basic functionality test

--- test/test_saber_settings/TestSaberSettings.cpp
+++ test/test_saber_settings/TestSaberSettings.cpp
@@ -9,8 +9,20 @@ void setUp() {
     sm = StateManager();
 }
 
+void test_settings_exist() {
+    Serial.begin(9600);
+    Serial.print("Channel: ");
+    Serial.println(sm.getChannel());
+    Serial.print("Sensitivity: ");
+    Serial.println(sm.getSensitivity());
+    Serial.print("Brightness: ");
+    Serial.println(sm.getBrightness());
+}
+
 int main() {
   UNITY_BEGIN();
+
+  RUN_TEST(test_settings_exist);
 
   UNITY_END();
 }
