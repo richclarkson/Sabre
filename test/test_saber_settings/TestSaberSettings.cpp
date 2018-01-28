df7cc21f6d541a2ed424dc7331259799b393a6a3
Steve Faletti <stevefaletti@gmail.com>
2018-01-28 11:58:14 -0500

    add eprom test rig

---
+++ test/test_saber_settings/TestSaberSettings.cpp
@@ -0,0 +1,18 @@
+#ifdef UNIT_TEST
+
+#include "StateManager.h"
+#include <unity.h>
+
+StateManager sm;
+
+void setUp() {
+    sm = StateManager();
+}
+
+int main() {
+  UNITY_BEGIN();
+
+  UNITY_END();
+}
+
+#endif
