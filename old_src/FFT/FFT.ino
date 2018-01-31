71d9146d92b41a0bf26e20c9325dd67a6cd097c0
Richard Clarkson <info@richardclarkson.com>
2018-01-31 09:44:14 -0500

    add description

--- old_src/FFT/FFT.ino
+++ old_src/FFT/FFT.ino
@@ -1,6 +1,18 @@
 
 //Adapted from: Spectrum Analyzer Basic Example
 
+/*
+   Key varriables and Outputs:
+
+     Sensitivity - from 0-8 to scale the sound readings accordingly
+
+     Mode - ripple vs other music modes, if ripple then ignor specific channel setting and use FFT to output all 8 channel values
+
+     Channel - all vs specific channel, if all then output RMS, if specific channel read and output only that channel
+
+     Level - the cleaned and scaled sound reading output 0-115 to feed into the LED code. (For ripple this is 8 values)
+
+*/
 
 #include <Audio.h>
 
@@ -14,7 +26,8 @@ AudioConnection          patchCord2(i2s1, 0, rms1, 0);
 
 
 // An array to hold the 8 frequency bands
-float level[8];    //TODO put bin numbers into an array here   
+float reading[8];    //TODO put bin numbers into an array here
+int level[8];
 
 
 // This is low-level noise that's subtracted from each frequency band:
@@ -24,17 +37,16 @@ static const int noise[8] = {   // numbers generated using serial plotter at roo
 
 int sensitivity = 7;  // 0-8 where 8 = maximum sensitivity
 
-
 static const int scale[9] = {
-  300, 600, 1000, 2000, 5000, 10000, 15000, 50000, 100000   // sensitivity setting (mulitplicatio factor)
-  };
+  300, 600, 1000, 2000, 5000, 10000, 15000, 50000, 100000   // sensitivity setting (mulitplication factor)
+};
 
 // Upper limit is used to clip readings off at a certian point set this as number of LEDs
-const int upperLimit = 115; 
+const int upperLimit = 115;
 
 // Lower threshold is used to make the readings more 'jumpy' at lower sensitvities.
 static const int lowerThreshold[9] = {
-  3, 2, 1, 0, 0, 0, 0, 0, 0  
+  3, 2, 1, 0, 0, 0, 0, 0, 0
 };
 
 
@@ -52,20 +64,20 @@ void loop() {
 
     // read the 512 FFT frequencies into 8 levels
 
-    level[0] =  fft1024.read(0, 1); //dark blue       // reading comes in a two decimal value from 0.00 to 1.00
-    level[1] =  fft1024.read(2);  // red
-    level[2] =  fft1024.read(3, 4); // green
-    level[3] =  fft1024.read(5, 8); // orange
-    level[4] =  fft1024.read(9, 26); // purple
-    level[5] =  fft1024.read(27, 98);  // grey
-    level[6] =  fft1024.read(99, 226); // light blue
-    level[7] =  fft1024.read(227, 511);  //black
+    reading[0] =  fft1024.read(0, 1); //dark blue       // reading comes in at between 0.0 to 1.0
+    reading[1] =  fft1024.read(2);  // red
+    reading[2] =  fft1024.read(3, 4); // green
+    reading[3] =  fft1024.read(5, 8); // orange
+    reading[4] =  fft1024.read(9, 26); // purple
+    reading[5] =  fft1024.read(27, 98);  // grey
+    reading[6] =  fft1024.read(99, 226); // light blue
+    reading[7] =  fft1024.read(227, 511);  //black
 
 
     for (int i = 0; i < 8; i++) {
-        
-        level[i] = level[i] - (noise[i]*0.0001);             // remove noise
-        level[i] = level[i] * scale[sensitivity];                // scalling
+
+      reading[i] = reading[i] - (noise[i] * 0.0001);           // remove noise
+      level[i] = reading[i] * scale[sensitivity];                // scale
 
       if (level[i] > upperLimit) {
         level[i] = upperLimit;       // limiting
@@ -74,7 +86,7 @@ void loop() {
         level[i] = 0;                             // limiting
       }
 
-     
+
       //Serial.print(level[i]);
       //Serial.print(" ");
 
@@ -85,8 +97,8 @@ void loop() {
 
     if (rms1.available()) {
 
-      int monoRms = (rms1.read()-0.0006) * scale[sensitivity];  //peak1.read outputs as a decimal from 1 (max) to 0 (min)
-      
+      int monoRms = (rms1.read() - 0.0006) * scale[sensitivity];     // remove noise and scale
+
       if (monoRms > upperLimit) {
         monoRms = upperLimit;      // limiting
       }
@@ -98,7 +110,7 @@ void loop() {
       Serial.print("     ");
     }
 
-   
+
     Serial.println(upperLimit);   // useful when stopping the serial plotter from autoscalling
   }
 }
