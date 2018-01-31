e207cab1e06745da8414126f6fc115e5a35409d4
Richard Clarkson <info@richardclarkson.com>
2018-01-31 13:41:37 -0500

    format and clean up code
    adjust scale values

--- old_src/FFT/FFT.ino
+++ old_src/FFT/FFT.ino
@@ -36,24 +36,24 @@ static const int noise[8] = {   // numbers generated using serial plotter at roo
   20, 05, 04, 06, 22, 64, 68, 89            // these are converted x 0.0001 later on before subtraction
 };
 
-int channel = 1;
+int channel = 8;
 
-static const int binStart[8] = {   // 
-  0, 2, 3, 5, 9, 27, 99, 227            // 
+static const int binStart[8] = {   //
+  0, 2, 3, 5, 9, 27, 99, 227            //
 };
 
-static const int binEnd[8] = {   // 
-  1, 2, 4, 8, 26, 98, 226, 511            // 
+static const int binEnd[8] = {   //
+  1, 2, 4, 8, 26, 98, 226, 511            //
 };
 
-int sensitivity = 5;  // 0-8 where 8 = maximum sensitivity
+int sensitivity = 4;  // 0-8 where 8 = maximum sensitivity
 
 static const int scale[9] = {
-  300, 1000, 2000, 5000, 7500, 10000, 15000, 50000, 100000   // sensitivity setting (mulitplication factor)
+  300, 1000, 3000, 6000, 10000, 20000, 30000, 50000, 100000   // sensitivity setting (mulitplication factor)
 };
 
 float eq[8] = {
-  1.5, 1.0, 1.0, 0.6, 1.0, 1.5, 2.0, 4.0   // individual channel scaling factor
+  1.0, 1.0, 0.7, 0.5, 1.0, 1.0, 1.5, 2.0   // individual channel scaling factor
 };
 
 
@@ -75,87 +75,98 @@ void setup() {
 
 void loop() {
 
-    checkbuttons();
+  checkbuttons();
 
-    if (channel < 8){     // channel specified and not ripple mode
-      FFTsingle();
-      Serial.print(level[channel]);
+  if (channel < 8) {    // channel specified and not ripple mode
+    FFTsingle();
+    Serial.print(level[channel]);
+    Serial.print(" ");
+    for (int i = 0; i < 7; i++) {
+      Serial.print(0);
       Serial.print(" ");
     }
+  }
 
-    else if (channel == 8){           // ripple mode
-      FFTall();
-      for (int i = 0; i < 8; i++) {
-        Serial.print(level[i]);
-        Serial.print(" ");
-      }
+  else if (channel == 8) {          // ripple mode
+    FFTall();
+    for (int i = 0; i < 8; i++) {
+      Serial.print(level[i]);
+      Serial.print(" ");
     }
+  }
 
-    else if (channel > 8){         //  channel not specified and not ripple mode
-      RMS();
-      Serial.print(monoRms);
-      Serial.print("     ");
-    }
-    
-
-    Serial.print(channel*10);      // feedback for testing rig
+  else if (channel == 9) {        //  channel not specified and not ripple mode
+    RMS();
+    Serial.print(monoRms);
     Serial.print("     ");
-    Serial.print(sensitivity*10);  // feedback for testing rig
-    Serial.print("     ");
-    Serial.println(upperLimit);    // useful when stopping the serial plotter from autoscalling
-    delay(10);
-  }
-
-
-
-
-
-
-
-  
-
-
-  void checkbuttons(){
-    int buttonState1 = digitalRead(2);
-    if (buttonState1 == LOW){
-      channel++;
-      if (channel > 9){ channel = 0;}
-      //Serial.println(channel);
-      delay(300);
-    }
-
-    
-    int buttonState2 = digitalRead(3);
-    if (buttonState2 == LOW){
-      sensitivity++;
-      if (sensitivity > 8){ sensitivity = 0;}
-      //Serial.println(sensitivity);
-      delay(300);
+    for (int i = 0; i < 7; i++) {
+      Serial.print(0);
+      Serial.print(" ");
     }
   }
 
 
+  Serial.print(channel * 10);    // feedback for testing rig
+  Serial.print("     ");
+  Serial.print(sensitivity * 10); // feedback for testing rig
+  Serial.print("     ");
+  Serial.println(upperLimit);    // useful when stopping the serial plotter from autoscalling
+  delay(10);
+}
 
-  void RMS(){
-      if (rms1.available()) {
 
-      monoRms = (rms1.read() - 0.0006) * scale[sensitivity];     // remove noise and scale
 
-      if (monoRms > upperLimit) {
-        monoRms = upperLimit;      // limiting
-      }
-      if (monoRms < 0) {
-        monoRms = 0;                            // limiting
-      }
 
-      //Serial.print(monoRms);
-      //Serial.print("     ");
+
+
+
+
+
+
+void checkbuttons() {
+  int buttonState1 = digitalRead(2);
+  if (buttonState1 == LOW) {
+    channel++;
+    if (channel > 9) {
+      channel = 0;
     }
+    delay(300);
   }
 
+  int buttonState2 = digitalRead(3);
+  if (buttonState2 == LOW) {
+    sensitivity++;
+    if (sensitivity > 8) {
+      sensitivity = 0;
+    }
+    delay(300);
+  }
+}
 
-  void FFTall(){
-    if (fft1024.available()) {
+
+
+
+void RMS() {
+  if (rms1.available()) {
+
+    monoRms = (rms1.read() - 0.0006) * scale[sensitivity];     // remove noise and scale
+
+    if (monoRms > upperLimit) {
+      monoRms = upperLimit;      // limiting
+    }
+    if (monoRms < 0) {
+      monoRms = 0;                            // limiting
+    }
+  }
+}
+
+
+
+
+
+
+void FFTall() {
+  if (fft1024.available()) {
 
     // read the 512 FFT frequencies into 8 levels
 
@@ -179,35 +190,32 @@ void loop() {
       if (level[i] < 0) {
         level[i] = 0;                             // limiting
       }
-
-      //Serial.print(level[i]);
-      //Serial.print(" ");
     }
   }
- }
+}
 
 
-  void FFTsingle(){
-    if (fft1024.available()) {
 
-      int i = channel;
 
-      reading[i] =  fft1024.read(binStart[i], binEnd[i]); 
 
-      reading[i] = reading[i] - (noise[i] * 0.0001);           // remove noise
-      level[i] = (reading[i] * scale[sensitivity]) * eq[i];                // scale
+void FFTsingle() {
+  if (fft1024.available()) {
 
-      if (level[i] > upperLimit) {
-        level[i] = upperLimit;       // limiting
-      }
-      if (level[i] < 0) {
-        level[i] = 0;                             // limiting
-      }
+    int i = channel;
 
-      //Serial.print(level[i]);
-      //Serial.print(" ");
+    reading[i] =  fft1024.read(binStart[i], binEnd[i]);
+
+    reading[i] = reading[i] - (noise[i] * 0.0001);           // remove noise
+    level[i] = (reading[i] * scale[sensitivity]) * eq[i];                // scale
+
+    if (level[i] > upperLimit) {
+      level[i] = upperLimit;       // limiting
+    }
+    if (level[i] < 0) {
+      level[i] = 0;                             // limiting
+    }
   }
- }
+}
 
 
 
