2a0abef72647195cd1ba7b388ba26808a52246e0
Steve Faletti <stevefaletti@gmail.com>
2018-02-12 20:39:58 -0500

    add setting states test

--- test/test_state_manager/TestStateManager.cpp
+++ test/test_state_manager/TestStateManager.cpp
@@ -13,6 +13,8 @@ StateManager sm;
 
 void setUp(void) { sm = StateManager(new TestOne, new ColorOne); }
 
+void setNonTestManager() { sm = StateManager(new NormalOff, new Neon); }
+
 void test_state_value_exists() {
   TEST_ASSERT_EQUAL(1, sm.getCurrent()->getTestVal());
 }
@@ -36,6 +38,36 @@ void test_color_advances_when_two_is_pressed() {
   TEST_ASSERT_EQUAL(10, sm.getColor()->getTestVal());
 }
 
+void test_channel_is_changed_when_pressed() {
+  setNonTestManager();
+  TEST_ASSERT_EQUAL(8, sm.getChannel());
+  sm.press();
+  sm.tap();
+  sm.press();
+  TEST_ASSERT_EQUAL(0, sm.getChannel());
+}
+
+void test_sensitivity_is_changed_when_pressed() {
+  setNonTestManager();
+  TEST_ASSERT_EQUAL(4, sm.getBrightness());
+  sm.press();
+  sm.tap();
+  sm.tap();
+  sm.press();
+  TEST_ASSERT_EQUAL(5, sm.getSensitivity());
+}
+
+void test_brightness_is_changed_when_pressed() {
+  setNonTestManager();
+  TEST_ASSERT_EQUAL(4, sm.getBrightness());
+  sm.press();
+  sm.tap();
+  sm.tap();
+  sm.tap();
+  sm.press();
+  TEST_ASSERT_EQUAL(5, sm.getBrightness());
+}
+
 int main() {
   UNITY_BEGIN();
 
@@ -43,6 +75,9 @@ int main() {
   RUN_TEST(test_state_advances_on_tap);
   RUN_TEST(test_color_exists);
   RUN_TEST(test_color_advances_when_two_is_pressed);
+  RUN_TEST(test_channel_is_changed_when_pressed);
+  RUN_TEST(test_sensitivity_is_changed_when_pressed);
+  RUN_TEST(test_brightness_is_changed_when_pressed);
 
   UNITY_END();
 }
